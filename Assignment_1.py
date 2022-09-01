87cda9f579c075eada467a8617af86a1c04611e4
Smruti Ranjan Lenka <81686733+SmrutiRanjan-L@users.noreply.github.com>
2022-09-01 22:21:46 +0530

    Add files via upload
    Using for loop please print all the prime numbers between 1-200 using FOR LOOP AND RANGE function

---
+++ Assignment_1.py
@@ -0,0 +1,12 @@
+minimum = 1
+maximum = 201
+
+for Number in range (minimum, maximum + 1):
+    count = 0
+    for i in range(2, (Number//2 + 1)):
+        if(Number % i == 0):
+            count = count + 1
+            break
+
+    if (count == 0 and Number != 1):
+        print(" %d" %Number, end = '  ')
